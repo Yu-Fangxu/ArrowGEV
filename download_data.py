9931d071b7bf23b33f1ba40d0cf156f53a8b8ea2
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2025-08-14 15:44:08 +0800

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 import json
 import random
-snapshot_download(repo_id="appletea2333/temporal_r1", local_dir="/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/time-r1/dataset/temporalr1", local_dir_use_symlinks=False, repo_type="dataset")
+snapshot_download(repo_id="appletea2333/temporal_r1", local_dir="", local_dir_use_symlinks=False, repo_type="dataset")
