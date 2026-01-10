f8ba90162a46016dca8824f0027827c1131294a5
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2026-01-10 14:57:19 +0800

    Update train_rl_SF.sh

--- scripts/posttrain/train_rl_SF.sh
+++ scripts/posttrain/train_rl_SF.sh
@@ -9,7 +9,7 @@ export PYTHONPATH=".:$PYTHONPATH"
 export DEBUG_MODE="true"
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 # 初始路径设置
-INIT_DATA_PATH="/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/dataset/ArrowGEV/annotations/train_2k5.json"
+INIT_DATA_PATH="./ArrowGEV/dataset/ArrowGEV/annotations/train_2k5.json"
 INIT_MODEL_PATH="Qwen/Qwen2.5-VL-7B-Instruct"
 
 for FILTER_INDEX in {0..0}; do
@@ -21,16 +21,16 @@ for FILTER_INDEX in {0..0}; do
         LOAD_MODEL_PATH=$INIT_MODEL_PATH
     else
         PREV_WANDB_NAME="${EXP_NAME}_0071_filter$((FILTER_INDEX-1))"
-        DATA_PATH=/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/logs/$EXP_NAME/$PREV_WANDB_NAME/filtering_epoch$((FILTER_INDEX-1))/train_v4_cloud_0071_all.json
-        LOAD_MODEL_PATH="/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/logs/$EXP_NAME/${PREV_WANDB_NAME}/train_epoch$((FILTER_INDEX-1))/$min_checkpoint_name"
+        DATA_PATH=./ArrowGEV/logs/$EXP_NAME/$PREV_WANDB_NAME/filtering_epoch$((FILTER_INDEX-1))/train_v4_cloud_0071_all.json
+        LOAD_MODEL_PATH="./ArrowGEV/logs/$EXP_NAME/${PREV_WANDB_NAME}/train_epoch$((FILTER_INDEX-1))/$min_checkpoint_name"
     fi
 
     # DATA_PATH=$INIT_DATA_PATH
     # LOAD_MODEL_PATH=$INIT_MODEL_PATH
 
     # 设置路径变量
-    OUTDIR=/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/logs/$EXP_NAME/${WANDB_NAME}/train_epoch$FILTER_INDEX
-    export LOG_PATH="/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/${OUTDIR}/log.txt"
+    OUTDIR=./ArrowGEV/logs/$EXP_NAME/${WANDB_NAME}/train_epoch$FILTER_INDEX
+    export LOG_PATH="./ArrowGEV/${OUTDIR}/log.txt"
     mkdir -p $OUTDIR
 
     # 训练阶段
@@ -39,12 +39,12 @@ for FILTER_INDEX in {0..0}; do
         --node_rank="0" \
         --master_addr="127.0.0.1" \
         --master_port="12371" \
-        /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/main.py \
-        --deepspeed /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/scripts/zero3_offload.json \
+        ./ArrowGEV/main.py \
+        --deepspeed ./ArrowGEV/scripts/zero3_offload.json \
         --output_dir $OUTDIR \
         --model_name_or_path $LOAD_MODEL_PATH \
         --train_data_path $DATA_PATH \
-        --video_folder /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/dataset/ArrowGEV/videos/timerft_data \
+        --video_folder ./ArrowGEV/dataset/ArrowGEV/videos/timerft_data \
         --dataset_name timerft \
         --max_prompt_length 8192 \
         --max_completion_length 20 \
@@ -73,11 +73,11 @@ for FILTER_INDEX in {0..0}; do
         --num_train_epochs 3 \
         # $([ $FILTER_INDEX -eq 0 ] && echo "5" || echo "1")
     # 推理阶段
-    # vllm_output_dir=/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/logs/$EXP_NAME/$WANDB_NAME/filtering_epoch$FILTER_INDEX
+    # vllm_output_dir=./ArrowGEV/logs/$EXP_NAME/$WANDB_NAME/filtering_epoch$FILTER_INDEX
     # mkdir -p "$vllm_output_dir"
 
     # # # 查找最小checkpoint
-    # checkpoints_parent_path="/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/logs/${EXP_NAME}/${WANDB_NAME}/train_epoch$FILTER_INDEX"
+    # checkpoints_parent_path="./ArrowGEV/logs/${EXP_NAME}/${WANDB_NAME}/train_epoch$FILTER_INDEX"
     # min_checkpoint_name=$(find "$checkpoints_parent_path" -maxdepth 1 -type d -name "checkpoint-*" -print0 | \
     #                     xargs -0 -n1 basename | \
     #                     awk -F'-' '{print $2, $0}' | \
@@ -93,7 +93,7 @@ for FILTER_INDEX in {0..0}; do
     # # 执行推理任务
     # for ((i=0; i<num_gpus; i++)); do
     #     gpu=${gpus[i]}
-    #     CUDA_VISIBLE_DEVICES=$gpu python /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/evaluate.py \
+    #     CUDA_VISIBLE_DEVICES=$gpu python ./ArrowGEV/evaluate.py \
     #         --model_base "$model_base" \
     #         --datatype tg \
     #         --batch_size 4 \
@@ -108,10 +108,10 @@ for FILTER_INDEX in {0..0}; do
     # done
     # wait
     # 数据处理
-    # python /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/src/vllm_inference/calc_difficulty.py --input $vllm_output_dir \
+    # python ./ArrowGEV/src/vllm_inference/calc_difficulty.py --input $vllm_output_dir \
     #     --split $DATA_PATH \
-    #     --output_dir "/mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/"
+    #     --output_dir "./ArrowGEV/"
     # # 生成过滤后的数据
-    # python /mnt/gemininjceph3/geminicephfs/pr-others-prctrans/fangxuyu/ArrowGEV/src/utils/process_data.py --input_json $vllm_output_dir/train_v4_cloud.json --task 0071_all
+    # python ./ArrowGEV/src/utils/process_data.py --input_json $vllm_output_dir/train_v4_cloud.json --task 0071_all
 
 done
